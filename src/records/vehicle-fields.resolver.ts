96d7c90580639f49a1d368a0e1c23afe70f0e593
Harsha Fernando <fernando.harsha2016@gmail.com.com>
2025-10-31 15:10:16 +0530

    add logs

--- src/records/vehicle-fields.resolver.ts
+++ src/records/vehicle-fields.resolver.ts
@@ -9,7 +9,13 @@ export class VehicleFieldsResolver {
 
   @ResolveField(() => [ServiceRecord])
   async serviceRecords(@Parent() vehicle: Vehicle): Promise<ServiceRecord[]> {
-    console.log('Resolving serviceRecords for VIN:', vehicle.vin);
-    return this.recordsService.findByVin(vehicle.vin);
+    console.log('Record Service: Federation request received');
+    console.log('Fetching service records for VIN:', vehicle.vin);
+    const records=await this.recordsService.findByVin(vehicle.vin);
+
+    console.log(`Found ${records.length} service records`);
+    console.log('Service records sent to gateway');
+
+    return records;
   }
 }
